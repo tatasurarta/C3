4da8cc6a081a6cbda6cb15a503274861b03a94e7
tatasurarta <91466068+tatasurarta@users.noreply.github.com>
2021-11-11 09:40:42 +0700

    Update mediainfo.py

--- bot/modules/mediainfo.py
+++ bot/modules/mediainfo.py
@@ -15,7 +15,7 @@ async def mediainfo(client, message):
     reply = message.reply_to_message
     if not reply:
         await message.reply_text(
-            "Balas ke Media dulu (hanya file/media telegram) bukan website"
+            "Bales ke Media dulu (cuma file/media telegram) bukan website"
         )
         return
     process = await message.reply_text("`Memproses...`")
@@ -36,7 +36,7 @@ async def mediainfo(client, message):
         if x_media is not None:
             break
     if x_media is None:
-        await process.edit_text("Membalas Format Media yang Valid")
+        await process.edit_text("Bales ke Format Media yang Bener")
         return
     media_type = str(type(x_media)).split("'")[1]
     file_path = safe_filename(await reply.download())
@@ -52,9 +52,8 @@ async def mediainfo(client, message):
 <h2>DETAILS</h2>
 <pre>{out or 'Not Supported'}</pre>
 """
-    title = 're-mirrorbot Mediainfo'
+    title = 'Rumah Awan Mediainfo'
     text_ = media_type.split(".")[-1].upper()
     link = post_to_telegraph(title, body_text)
-    
     markup = InlineKeyboardMarkup([[InlineKeyboardButton(text=text_, url=link)]])
     await process.edit_text("ℹ️ <b>M𝐌𝐄𝐃𝐈𝐀 𝐈𝐍𝐅𝐎 𝐁𝐮𝐚𝐭 @{message.from_user.username} </b>", reply_markup=markup)
